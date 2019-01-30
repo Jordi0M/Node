fbf694328375bd766adb7c509ba95c9b8a9b65fa
jordi <jmartinezmateos@iesesteveterradas.cat>
2019-01-30 20:16:54 +0100

    añadiendo cosas

--- libreria/express/express.js
+++ libreria/express/express.js
@@ -2,7 +2,11 @@ var express = require("express")
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World! <br><a href="/prueba">Caro</a>');
+});
+
+app.get('/prueba', function (req, res) {
+  res.send('Hello World v2 <br><a href="/..">Caro</a>');
 });
 
 app.listen(3000, function () {
